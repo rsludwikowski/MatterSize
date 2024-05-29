8fb9a04a278aa6e6b6c375ef15c6258311e3c1e4
Janusz Zychal <56130048+bigjohn98pl@users.noreply.github.com>
2024-05-29 19:25:45 +0200

    - clean code

--- scripts/S_Planet.gd
+++ scripts/S_Planet.gd
@@ -1,113 +1,31 @@
 extends RigidBody3D 
 
-var Universe = load("res://scripts/Universe.gd")
+var universe = load("res://scripts/universe.gd")
 
 # Zmienna do kontrolowania prędkości ruchu
-var speed = 10.0
+@export var speed = 10.0
 # Zmienna do kontrolowania siły
-var move_force = 10.0
+@export var move_force = 10.0
+@export var initialVelocity = Vector3.ZERO
 var direction = Vector3.ZERO
 var new_transform = self.global_transform
-var initialVelocity = Vector3.ZERO
-
 
 
 func UpdateVelocity(delta_T):
-	var otherPlanets = get_all_rb3d(get_tree().root)
+	var otherPlanets = get_all_rb3d()
 	for planet in otherPlanets:
 		var sqrDst = (planet.global_position - self.global_position).length_squared()
 		var forceDir = (planet.global_position - self.global_position).normalized()
-		var force = forceDir * Universe.G_constant
+		var force = forceDir * universe.G_CONSTANT
 		var acceleration = force / self.mass
 		self.linear_velocity += acceleration * delta_T
 		#print(self.linear_velocity)
-		
-		
 
 func UpdatePosition(delta_T):
 	self.position += self.linear_velocity * delta_T
 
-
 func _ready():
-	# Inicjalizacja skryptu
-	self.set_meta("initialVelocity",initialVelocity)
-	#print(self.get_meta("metadata/initialVelocity"))
-	if initialVelocity != Vector3.ZERO:
-		self.angular_velocity = initialVelocity
 	pass
 
-#
-#func _physics_process(delta):
-	#
-	##direction = Vector3.ZERO
-	##
-	##
-	##if Input.is_action_pressed("ui_right"):
-		##direction.x += 1
-	##if Input.is_action_pressed("ui_left"):
-		##direction.x -= 1
-	##if Input.is_action_pressed("ui_down"):
-		##direction.z += 1
-	##if Input.is_action_pressed("ui_up"):
-		##direction.z -= 1
-	##
-	###if direction != Vector3.ZERO:
-		###var new_transform = self.global_transform
-		###new_transform.origin += direction*delta
-		###self.global_transform = new_transform
-	####move_rigidbody_to(new_transform)
-	##move_and_collide(direction*delta)
-	#
-	#UpdateVelocity(get_all_rb3d(get_tree().root),delta)
-	#UpdatePosition(delta)
-#
-	#
-	#
-	#
-	##
-##func integrate_forces(state):
-	##direction = Vector3.ZERO
-	##
-	##if Input.is_action_just_pressed("ui_select"):
-		##print("SPacja kliknięta")
-		##print(get_all_rb3d(get_tree().root))
-	##
-	##
-	##if Input.is_action_pressed("ui_right"):
-		##direction.x += 1
-	##if Input.is_action_pressed("ui_left"):
-		##direction.x -= 1
-	##if Input.is_action_pressed("ui_down"):
-		##direction.z += 1
-	##if Input.is_action_pressed("ui_up"):
-		##direction.z -= 1
-##
-##
-##
-	##var new_transform = self.global_transform
-	##new_transform.origin += direction
-	##self.global_transform = new_transform
-	### Normalizowanie kierunku, aby ruch nie był szybszy po przekątnej
-	##if direction.length() > 0:
-		##direction = direction.normalized() * move_force
-##
-	### Dodanie siły do obiektu
-	####apply_central_force(direction)
-##
-	### Ograniczenie prędkości obiektu
-	##if linear_velocity.length() > speed:
-		##linear_velocity = linear_velocity.normalized() * speed
-#
-
-
-
-func get_all_rb3d(node):
-	var objects = []
-	
-	for child in node.get_children():
-		
-		if child is RigidBody3D and child != self:
-			objects.append(child)
-			#print("test2")
-		objects += get_all_rb3d(child)  # Rekurencyjne wywołanie funkcji dla dzieci
-	return objects
+func get_all_rb3d():
+	return get_tree().get_nodes_in_group("Planets")
--- scripts/Universe.gd
+++ scripts/Universe.gd
@@ -1,49 +1,20 @@
 extends Node
 
-const G_constant = 6.67
+const G_CONSTANT = 6.67
 
 var planets = []
 
-
-func get_all_planets(node,script_path):
-	var objects = []
-	
-	for child in node.get_children():
-		
-		if child is RigidBody3D and child.get_script() and child.get_script().resource_path == script_path:
-			objects.append(child)
-			#print("test2")
-		objects += get_all_planets(child,script_path)  # Rekurencyjne wywołanie funkcji dla dzieci
-	return objects
-
-
-
-func find_node_with_script(node, script_path):
-	if node.get_script() and node.get_script().resource_path == script_path:
-		return node
-	
-	for child in node.get_children():
-		var result = find_node_with_script(child, script_path)
-		if result:
-			return result
-	
-	return null
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var target_script_path = "res://scripts/S_Planet.gd"
-	#var target_node = find_node_with_script(get_tree().root, target_script_path)
-	planets = get_all_planets(get_tree().root,target_script_path)
+	planets = get_tree().get_nodes_in_group("Planets")
 	print(planets)
-	 # Replace with function body.
 
 func get_G():
-	return G_constant
+	return G_CONSTANT
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
-
 
 func _physics_process(delta):
 	for planet in planets:
