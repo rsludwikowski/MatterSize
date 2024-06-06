1f9b8e2d7cceffc60a803f64c2782df1944b5dbc
Robert Szott-Ludwikowski <72131712+rsludwikowski@users.noreply.github.com>
2024-06-06 22:31:37 +0200

    -fixing cursor

--- scripts/cursor.gd
+++ scripts/cursor.gd
@@ -1,9 +1,11 @@
 
 extends Control
 
-
+@export var camera_frame: PhantomCamera3D
+@export var camera_group: PhantomCamera3D
 
 var mouse_pos
+var active_camera :PhantomCamera3D
 
 
 # Called when the node enters the scene tree for the first time.
@@ -13,13 +15,26 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	
+	if(camera_frame.priority>camera_group.priority):
+		#print("CAMERA_FRAME")
+		active_camera = camera_frame
+	else:
+		#print("CAMERA_GROUP")
+		active_camera = camera_group
+	
+	
+	
+	
 	mouse_pos = get_viewport().get_mouse_position()
 
 	print(mouse_pos)
 	var camera = $"../Cameras/camera_space"
 	
+	print(camera.global_position)
 	#ray casting positions
 	mouse_pos = camera.project_ray_origin(mouse_pos)
+	print(mouse_pos)
 	
 	
 	
