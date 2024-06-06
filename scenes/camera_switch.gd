afafe46d65e894a7ec8fa0ca9ff5168f64e87839
Janusz Zychal <56130048+bigjohn98pl@users.noreply.github.com>
2024-06-06 22:33:16 +0200

    - added function to set group camera targets to all planets and player

--- scenes/camera_switch.gd
+++ scenes/camera_switch.gd
@@ -4,7 +4,7 @@ extends Node
 @export var camera_group: PhantomCamera3D
 
 func _ready():
-	pass
+	set_group_camera_to_all_planets()
 
 func _process(delta):
 	toggle_camera()
@@ -14,7 +14,19 @@ func toggle_camera():
 		camera_frame.priority = 0
 		camera_group.priority = 20
 	elif Input.is_action_pressed("camera_group"):
+		set_group_camera_to_all_planets()
 		camera_frame.priority = 20
 		camera_group.priority = 0
 	else:
 		pass
+
+func set_group_camera_to_all_planets():
+	var targets_node_3d: Array[Node3D] = []
+	var targets = get_tree().get_nodes_in_group("Planets")
+	targets += get_tree().get_nodes_in_group("Player")
+	print(targets)
+	
+	for target in targets:
+		var target_node = target as Node3D
+		targets_node_3d.append(target_node)
+	camera_group.set_follow_targets(targets_node_3d)
