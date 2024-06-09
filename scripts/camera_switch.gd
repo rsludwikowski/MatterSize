19d00f3c118d45f07811e7211230b0a9e03c157b
Janusz Zychal <56130048+bigjohn98pl@users.noreply.github.com>
2024-06-09 23:53:28 +0200

    - all 3 cameras have zoom

--- scripts/camera_switch.gd
+++ scripts/camera_switch.gd
@@ -1,9 +1,9 @@
 extends Node
 
 @export var zoom_increment: int = 10
-@export var camera_1_frame: PhantomCamera3D
-@export var camera_2_group: PhantomCamera3D
-@export var camera_3_frame_sun: PhantomCamera3D
+@export var camera_1_player: PhantomCamera3D
+@export var camera_2_planets: PhantomCamera3D
+@export var camera_3_sun: PhantomCamera3D
 @export var host: PhantomCameraHost
 
 var active_camera = "camera_3"
@@ -11,9 +11,10 @@ var zoom_min: int = 0
 var zoom_max: int = 1000
 
 @onready var cameras: Dictionary = {
-	"camera_1": camera_1_frame,
-	"camera_2": camera_2_group,
-	"camera_3": camera_3_frame_sun}
+	"camera_1": camera_1_player,
+	"camera_2": camera_2_planets,
+	"camera_3": camera_3_sun
+	}
 	
 func _ready():
 	host._follow_target_physics_based = true
@@ -41,28 +42,51 @@ func toggle_camera():
 		set_group_camera_to_all_planets()
 	
 	# Debugging: wyświetlenie priorytetów kamer
-	for cam in cameras:
-		print(cam, "priority:", cameras[cam].priority)
+	#for cam in cameras:
+		#print(cam, "priority:", cameras[cam].priority)
 		
 func set_camera_zoom(camera):
 	var zooming = 0
+	var is_zooming = false
 	if Input.is_action_just_pressed("zoom_in"):
 		zooming = zoom_increment
+		is_zooming = true
 	elif Input.is_action_just_pressed("zoom_out"):
 		zooming = -zoom_increment
+		is_zooming = true
 		
-	if camera == "camera_2":
-		var distance = cameras[camera].auto_follow_distance_max
-		distance += zooming
-		if distance > zoom_min and distance < zoom_max:
-			cameras[camera].auto_follow_distance_max += zooming
-	else:
-		var distance = cameras[camera].get_follow_distance()
-		distance += zooming
-		if distance > zoom_min and distance < zoom_max:
-			cameras[camera].set_follow_distance(distance)
-	print(cameras[camera].get_follow_distance())
+	if true: 
+		if cameras[camera].follow_mode == PhantomCamera3D.FollowMode.GROUP:
+			var distance = cameras[camera].auto_follow_distance_max
+			distance += zooming
+			if is_in_range(distance):
+				cameras[camera].auto_follow_distance_max = distance
+				print(cameras[camera].auto_follow_distance_max)
+		elif cameras[camera].follow_mode == PhantomCamera3D.FollowMode.FRAMED:
+			var distance = cameras[camera].follow_distance
+			distance += zooming
+			if is_in_range(distance):
+				cameras[camera].follow_distance = distance
+				print(cameras[camera].follow_distance)
+		elif cameras[camera].follow_mode == PhantomCamera3D.FollowMode.THIRD_PERSON:
+			var distance = cameras[camera].spring_length
+			distance += zooming
+			if is_in_range(distance):
+				cameras[camera].spring_length = distance
+				print(cameras[camera].follow_distance)
+		else:
+			var distance = cameras[camera].follow_offset.z
+			distance += zooming
+			if is_in_range(distance):
+				cameras[camera].follow_offset.z = distance
+				print(cameras[camera].follow_offset.z)
 	
+func is_in_range(distance):
+	if distance > zoom_min and distance < zoom_max:
+		return true
+	else:
+		return false
+		
 func set_group_camera_to_all_planets():
 	var targets_node_3d: Array[Node3D] = []
 	var targets = get_tree().get_nodes_in_group("Planets")
